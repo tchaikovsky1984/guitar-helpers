c71cde50cbcf668a31eee94550ca24b78f4b290f
tchaikovsky1984 <anshulkumarg0re@gmail.com>
2026-04-19 10:35:31 +0530

    feat(note_helper): react init

---
+++ note_helper/frontend/src/App.tsx
@@ -0,0 +1,183 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [sharps, setSharps] = useState(true);
+  const [time, setTime] = useState(1);
+  const [note, setNote] = useState("E");
+
+  let notes: string[] = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
+
+  const handleCheckboxChange = () => {
+    setSharps(!sharps);
+  };
+  const handleNumberChange = (e) => {
+    const target = e.target;
+    const value = target.value;
+    setTime(value);
+  };
+
+  async function startLoop(e) {
+    e.preventDefault();
+    if (!sharps) {
+      notes = notes.filter((note) => note.length === 1);
+    }
+    while (true) {
+      const randomIdx = Math.floor(Math.random() * (notes.length - 1));
+      setNote(notes[randomIdx]);
+      await new Promise(resolve => setTimeout(resolve, time * 1000));
+    }
+  }
+
+  return (
+    <>
+      <section id='center'>
+        <div>
+          <h1>Note Helper</h1>
+          <form>
+
+            <label>Include Sharps?:</label>
+            <input
+              type='checkbox'
+              name='sharps'
+              checked={sharps}
+              onChange={handleCheckboxChange}
+            />
+
+            <label>Time Between Prompts:</label>
+            <input
+              type='number'
+              name='time'
+              defaultValue={time}
+              onChange={handleNumberChange}
+            />
+
+            <button
+              onClick={startLoop}
+            >Start</button>
+          </form>
+        </div>
+        <div>
+          <h2>{note}</h2>
+        </div>
+      </section>
+    </>
+  );
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" />
+        </div>
+        <div>
+          <h1>Get started</h1>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
+          </p>
+        </div>
+        <button
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
+      </section>
+
+      <div className="ticks"></div>
+
+      <section id="next-steps">
+        <div id="docs">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#documentation-icon"></use>
+          </svg>
+          <h2>Documentation</h2>
+          <p>Your questions, answered</p>
+          <ul>
+            <li>
+              <a href="https://vite.dev/" target="_blank">
+                <img className="logo" src={viteLogo} alt="" />
+                Explore Vite
+              </a>
+            </li>
+            <li>
+              <a href="https://react.dev/" target="_blank">
+                <img className="button-icon" src={reactLogo} alt="" />
+                Learn more
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div id="social">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#social-icon"></use>
+          </svg>
+          <h2>Connect with us</h2>
+          <p>Join the Vite community</p>
+          <ul>
+            <li>
+              <a href="https://github.com/vitejs/vite" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#github-icon"></use>
+                </svg>
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a href="https://chat.vite.dev/" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#discord-icon"></use>
+                </svg>
+                Discord
+              </a>
+            </li>
+            <li>
+              <a href="https://x.com/vite_js" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#x-icon"></use>
+                </svg>
+                X.com
+              </a>
+            </li>
+            <li>
+              <a href="https://bsky.app/profile/vite.dev" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#bluesky-icon"></use>
+                </svg>
+                Bluesky
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+
+}
+
+export default App
